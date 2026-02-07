0a03b0c9534ceebe67cf301fd4309a8aae5299b7
RishHUB447 <rishabh447verma@gmail.com>
2026-02-07 18:48:08 +0530

    features 3.0

--- Methods.py
+++ Methods.py
@@ -1,17 +1,14 @@
-class Methods:
-    def Sumnatural(self, n):
-        return (n * (n + 1)) // 2
-    
-    def Factorial(self, n):
-        if n == 0 or n == 1:
-            return 1
-        else:
-            return n * self.Factorial(n - 1)
-        
-    def Fibonacci(self, n):
-        if n <= 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return self.Fibonacci(n - 1) + self.Fibonacci(n - 2)
+def sum_natural(n):
+    return n * (n + 1) // 2
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
---
+++ main.py
@@ -0,0 +1,3 @@
+import Methods as m
+
+print("Sum of first 5 natural numbers:", m.sum_natural(5))
